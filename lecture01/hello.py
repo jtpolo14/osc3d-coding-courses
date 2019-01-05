136f31ce3a840913458c8f58d8e1dc5d1bf988fb
JT Thomas <jtpolo14@gmail.com>
2019-01-05 12:31:39 -0600

    updated readme

---
+++ lecture01/hello.py
@@ -0,0 +1,7 @@
+# This line is a comment 
+# All comments begin with a '#'
+# Comments are ignored by python
+
+# this print function outputs the 
+# text Hello World to the screen
+print('Hello World')
