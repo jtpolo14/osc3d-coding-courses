a523e72b9871c22ab46fbf410e60a067a64f5a96
TheTides <luke.milby@gmail.com>
2019-01-17 16:44:35 -0600

    l04 code

---
+++ RaspberryPi/lecture04/data_types.py
@@ -0,0 +1,35 @@
+# String
+my_string = "Hello World"
+print(type(my_string))
+
+# Number
+## Integer
+my_int = 80
+print(type(my_int))
+# Converting string to integer
+my_string_int = "80"
+print(type(int(my_string_int)))
+# ValueError | Error converting non-integer string
+print(type(int(my_string)))
+
+
+# Float
+my_float = 10.0
+
+# List
+# Find each element of the list through and index
+my_list = ["Hello", "World"]
+print(type(my_list))
+
+
+# Tuple
+## Imutable
+## Once establisted
+my_tuple = (0,1)
+
+# Dictionary
+#
+my_dictionary = {
+    "message": "Hello World!",
+    "number": 80
+}
