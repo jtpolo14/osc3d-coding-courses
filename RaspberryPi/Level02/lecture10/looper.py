734b8ee3e12f18f9678d7f7face5ea918ee00e30
JT Thomas <jtpolo14@gmail.com>
2019-04-09 18:17:11 -0500

    added lecture10

---
+++ RaspberryPi/Level02/lecture10/looper.py
@@ -0,0 +1,27 @@
+################
+# Challenge: 1 million iterations
+#
+# This coding challenge requires you to update the below psuedo code.
+# This means the below code has only been partially setup for you, and 
+# WILL NOT WORK! You will need to make changes to the code to fix it.
+#  
+# Read the requirements and psuedo comments. GOOD LUCK!!!!!!!!!!
+#
+#
+# Requirements:
+#	- Create an looping program
+#	- print out all even numbers from 0 to 1 million
+#	- no odd numbers should be printed
+#
+################
+
+# create a for loop
+# REMEMBER: the range function 
+for rename_me in range(rename_me):
+    # this code will run each time the loop is executed
+    ADD CODE HERE
+
+    #print only even numbers
+    # make sure that this line does not run when
+    # the input is odd
+    print(rename_me)
