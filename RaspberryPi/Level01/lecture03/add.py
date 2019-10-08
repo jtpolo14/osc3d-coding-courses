53a2aea8a4500598f019367c1fb088da6dbe0ad4
JT Thomas <jtpolo14@gmail.com>
2019-10-07 20:07:25 -0500

    Fixed Comments

--- RaspberryPi/Level01/lecture03/add.py
+++ RaspberryPi/Level01/lecture03/add.py
@@ -1,15 +1,15 @@
 # use the built-in python function 'input'
 # to get a number from the user. The number
-# will be stored in a variable named 'number1'
+# will be stored in a variable named 'string_number1'
 string_number1 = input('Enter a number: ')
 
 # use the built-in python function 'input'
 # to get a number from the user. The number
-# will be stored in a variable named 'number2'
+# will be stored in a variable named 'string_number2'
 string_number2 = input('Enter a number: ')
 
 integer_answer = int(string_number1) + int(string_number2)
 
 # use the built-in python function print to output
-# the variable 'the_word' 5 times to the screen
+# the variable 'integer_answer' 5 times to the screen
 print("The answer is", integer_answer)
