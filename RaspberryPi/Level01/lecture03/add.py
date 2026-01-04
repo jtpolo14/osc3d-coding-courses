083303321c56fd198407a94e938c64aa9ae26926
Cam Parham <113310295+camparham@users.noreply.github.com>
2026-01-04 15:43:41 -0500

    Add instructional language for converting a string to an integer
    Added instructional language for converting a string to an integer and added a line to print (integer_answer), so the last line of code could print.

--- RaspberryPi/Level01/lecture03/add.py
+++ RaspberryPi/Level01/lecture03/add.py
@@ -8,7 +8,9 @@ string_number1 = input('Enter a number: ')
 # will be stored in a variable named string_number2
 string_number2 = input('Enter a number: ')
 
+# convert the string into an integer so the numbers can be added together
 integer_answer = int(string_number1) + int(string_number2)
+print(integer_answer)
 
 # use the built-in python function print to output
 # the variable integer_answer 5 times to the screen
