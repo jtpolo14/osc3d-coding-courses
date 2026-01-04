b42bccd47a81c3fb15096aecade42d179268eba2
Cam Parham <113310295+camparham@users.noreply.github.com>
2026-01-04 16:29:16 -0500

    Fix string escape examples and type conversion
    Updated comments and corrected string conversion example.

--- RaspberryPi/Level01/lecture04/data_types.py
+++ RaspberryPi/Level01/lecture04/data_types.py
@@ -1,10 +1,13 @@
 # String literals
+# To identify the data type, you write print(type).
 my_string = "Hello World"
 print(type(my_string))
 
 # String escape characters \'
 # Example 1
 # Wrong
+# This is wrong because you can't have three single quotes on one command line. 
+# You either have to use double quotes for the entire command line or use single quotes, and when you need an apostrophe, you add a backslash, then an apostrophe. 
 #print('She's been to Japan twice')
 # Example 2
 # Correct
@@ -17,7 +20,7 @@ print(type(my_int))
 my_string_int = "80"
 print(type(int(my_string_int)))
 # ValueError | Error converting non-integer string
-print(type(int(my_string)))
+print(type(int(my_string_int)))
 
 # Float
 # value containing a decimal point
